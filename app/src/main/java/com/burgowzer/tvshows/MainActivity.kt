cfdc1aaf8ec6bd86cbf3f9dc527e0195df09aeb7
Burgowzer <Burgowzer@users.noreply.github.com>
2020-09-26 23:21:19 +0200

    Addition of JSON request

--- app/src/main/java/com/burgowzer/tvshows/MainActivity.kt
+++ app/src/main/java/com/burgowzer/tvshows/MainActivity.kt
@@ -2,10 +2,22 @@ package com.burgowzer.tvshows
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var requestQueue:RequestQueue
+    val url:String = "http://jsonplaceholder.typicode.com/todos"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,6 +25,33 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = CardViewAdapter()
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        requestQueue = Volley.newRequestQueue(this)
+
+        famousButton.setOnClickListener {
+
+            UpdateListOfTitles(url)
+
+        }
+    }
+
+    fun UpdateListOfTitles(url:String){
+
+        val request = JsonArrayRequest(Request.Method.GET,url,null, { ObjectList:JSONArray ->
+
+            val _titleList = mutableListOf<JSONObject>()
+            for (taskIndex in 0 until ObjectList.length()){
+                val singleObject = ObjectList.getJSONObject(taskIndex)
+                _titleList.add(singleObject)
+
+        }
+        },
+            { Toast.makeText(this,"ERROR",Toast.LENGTH_SHORT) })
+
+        requestQueue.add(request)
+
 
     }
+
+
+
 }
